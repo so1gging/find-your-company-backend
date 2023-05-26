af93b42288e7ac619773325f883fc37286fb9acc
김솔지 <so1gging@gmail.com>
2023-05-27 00:31:59 +0900

    크롤링 임시 코드 작업중

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,14 +1,58 @@
 import { Injectable } from '@nestjs/common'
+import * as puppeteer from 'puppeteer'
+import * as cherrio from 'cheerio'
 import { CreateCompanyDto } from './dto/create-company.dto'
 import { UpdateCompanyDto } from './dto/update-company.dto'
 
 @Injectable()
 export class CompanyService {
+  url =
+    'https://www.wanted.co.kr/wdlist/518?country=kr&job_sort=company.response_rate_order&years=0&years=3&selected=873&selected=669&locations=seoul.gangnam-gu'
+
+  wait = (ms) => new Promise((res) => setTimeout(res, ms))
   create(createCompanyDto: CreateCompanyDto) {
     return 'This action adds a new company'
   }
 
-  findAll() {
+  async findAll() {
+    const browser = await puppeteer.launch({
+      headless: false,
+    })
+
+    const page = await browser.newPage()
+    await page.setViewport({
+      width: 1200,
+      height: 600,
+    })
+
+    await page.goto(this.url)
+
+    const lastHeight = await page.evaluate('document.body.scrollHeight')
+
+    // while (true) {
+    //   await page.evaluate('window.scrollTo(0, document.body.scrollHeight)')
+    //   await this.wait(500) // sleep a bit
+    //   const newHeight = await page.evaluate('document.body.scrollHeight')
+    //   if (newHeight === lastHeight) {
+    //     break
+    //   }
+    //   lastHeight = newHeight
+    // }
+
+    const content = await page.content()
+    //
+    const $ = cherrio.load(content)
+    const list = await page.$('#__next ul[data-cy="job-list"]')
+
+    const evalDate = await list.evaluate((element) => {
+      const aTag = $(element).find('li > div > a')
+      console.log(element.innerHTML)
+      // return aTag.attr('href')
+      return element.innerHTML
+    }, list)
+
+    console.log(evalDate)
+
     return `This action returns all company`
   }
 
